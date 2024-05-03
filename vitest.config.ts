95ac7db88a79e6d8d67a0a9e4e0c7b1eba4879ce
Yuliya Karuk <karuk.yuliya@gmail.com>
2024-05-03 17:39:33 +0300

    refactor: simplify folder structure

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/__test__/setup.ts',
+    setupFiles: './src/test/setup.ts',
     coverage: {
       provider: 'v8',
     },
