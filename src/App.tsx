63d7f8431ecd7c0c91d22a79661cf786558827ed
Binarion <itmaster@duck.com>
2024-05-06 05:23:20 +0300

    feat: folder for styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '../public/vite.svg';
-import './App.css';
+import './styles/App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
