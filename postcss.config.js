072c8af266b14c104c3e544ed68fe5ede79cc42b
Binarion <itmaster@duck.com>
2024-05-06 23:31:27 +0300

    fix: remove @types/node

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 export default {
-   plugins: {
-      autoprefixer: {
-         grid: true,
-         overrideBrowserslist: ["last 5 versions"],
-         cascade: true,
-      },
-   },
+  plugins: {
+    autoprefixer: {
+      grid: true,
+      overrideBrowserslist: ['last 5 versions'],
+      cascade: true,
+    },
+  },
 };
