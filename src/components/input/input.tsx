0098b56536c40dcc683d132af7f38e0c4fa6585f
Vital <33701337+Vitali86@users.noreply.github.com>
2024-05-14 12:47:11 +0300

    Feature/rss ecomm 14 login page (#20)
    * feat: implement login page
    
    * feat: implement show password checkbox
    
    * save: save bug with email input field
    
    * feat: implement login page
    
    * refactor: refactor according to the comments
    
    * refactor: move the input field to a separate component
    
    * refactor: del ```generateScopedName: '[local]'``` and add eslint rule 'react/require-default-props': 'off'

---
+++ src/components/input/input.tsx
@@ -0,0 +1,30 @@
+import { FieldValues, Path, RegisterOptions, UseFormRegister } from 'react-hook-form';
+import styles from './input.module.scss';
+
+interface InputProps<T extends FieldValues> {
+  name: Path<T>;
+  label: string;
+  register: UseFormRegister<T>;
+  validationSchema: RegisterOptions;
+  type?: string;
+  placeholder?: string;
+}
+
+export function Input<T extends FieldValues>(props: InputProps<T>) {
+  const { name, label, register, validationSchema, type = 'text', placeholder = '' } = props;
+
+  return (
+    <>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        className={styles.input}
+        id={name}
+        type={type}
+        placeholder={placeholder}
+        {...register(name, validationSchema)}
+      />
+    </>
+  );
+}
