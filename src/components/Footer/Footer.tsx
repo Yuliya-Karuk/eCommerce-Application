365427e3733872dbf8331e2253d63ee4e2c977af
Maxim Ravinskiy <itmaster@duck.com>
2024-05-20 19:30:50 +0300

    refactor: footer tag

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,5 +2,5 @@ import { FC } from 'react';
 import styles from './Footer.module.scss';
 
 export const Footer: FC = () => {
-  return <div className={styles.footer} />;
+  return <footer className={styles.footer} />;
 };
