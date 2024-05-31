81fa0456bf1732a8cecb60c05043df7b075d91fd
Vitali86 <vitali86@tut.by>
2024-05-31 10:46:47 +0300

    feat: refine ProfilePassword

--- src/components/ProfilePassword/ProfilePassword.tsx
+++ src/components/ProfilePassword/ProfilePassword.tsx
@@ -1,9 +1,8 @@
 import eyeOff from '@assets/eye-off.svg';
 import eyeOn from '@assets/eye-show.svg';
-import { Customer, CustomerDraft } from '@commercetools/platform-sdk';
+import { Customer, MyCustomerChangePassword } from '@commercetools/platform-sdk';
 import { Input } from '@components/Input/Input';
 import { passwordValidationRules } from '@utils/validationConst';
-import classnames from 'classnames';
 import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import styles from './ProfilePassword.module.scss';
@@ -19,31 +18,44 @@ export const ProfilePassword = ({ customerData }: ProfilePasswordProps) => {
     setValue,
     // watch,
     formState: { errors, isValid },
-  } = useForm<CustomerDraft>({ mode: 'all' });
+  } = useForm<MyCustomerChangePassword>({ mode: 'all' });
 
-  const [isEditing, setIsEditing] = useState(false);
-  const [dataEdited, setDataEdited] = useState(false);
-  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [dataEdited, setDataEdited] = useState(false);
+  const [isCurrentPasswordVisible, setIsCurrentPasswordVisible] = useState(false);
+  const [isNewPasswordVisible, setIsNewPasswordVisible] = useState(false);
 
   // const watchedFields = watch(['password']);
 
-  const setInputs = () => {
-    setValue('password', '');
+  const resetPasswordFields = () => {
+    setValue('currentPassword', '');
+    setValue('newPassword', '');
   };
 
-  const resetChanges = () => {
-    setInputs();
-    setDataEdited(false);
-    setIsEditing(!isEditing);
-  };
+  // const resetChanges = () => {
+  //   setInputs();
+  //   // setDataEdited(false);
+  //   setIsEditing(!isEditing);
+  // };
 
-  const onSubmitPassword = () => {
-    console.log('gdgd');
+  const onSubmitPassword = (data: MyCustomerChangePassword) => {
+    const changePswdRequest = {
+      version: 1, // The current version of the customer's data
+      actions: [
+        {
+          action: 'changePassword',
+          ...data,
+        },
+      ],
+    };
+
+    console.log(changePswdRequest);
+    resetPasswordFields();
   };
 
   useEffect(() => {
-    setInputs();
-    setDataEdited(true);
+    resetPasswordFields();
+    // setDataEdited(true);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [customerData]);
 
@@ -51,48 +63,51 @@ export const ProfilePassword = ({ customerData }: ProfilePasswordProps) => {
     <form onSubmit={handleSubmit(onSubmitPassword)} className={styles.form}>
       <div className={styles.inputContainer}>
         <Input
-          name="password"
-          label="Password"
-          type={isPasswordVisible ? 'text' : 'password'}
+          name="currentPassword"
+          label="Current password:"
+          type={isCurrentPasswordVisible ? 'text' : 'password'}
           register={register}
           validationSchema={passwordValidationRules}
-          isInvalid={!!errors.password}
-          required
+          isInvalid={!!errors.currentPassword}
           autocomplete="current-password"
         />
 
-        <button type="button" onClick={() => setIsPasswordVisible(!isPasswordVisible)} className={styles.eye}>
-          <img src={isPasswordVisible ? eyeOn : eyeOff} alt="eye" />
+        <button
+          type="button"
+          onClick={() => setIsCurrentPasswordVisible(!isCurrentPasswordVisible)}
+          className={styles.eye}
+        >
+          <img src={isCurrentPasswordVisible ? eyeOn : eyeOff} alt="eye" />
         </button>
 
-        <p className={styles.passwordError}>{errors?.password?.message}</p>
+        <p className={styles.error}>{errors?.currentPassword?.message}</p>
       </div>
       <div className={styles.inputContainer}>
         <Input
-          name="password"
-          label="Password"
-          type={isPasswordVisible ? 'text' : 'password'}
+          name="newPassword"
+          label="New-password"
+          type={isNewPasswordVisible ? 'text' : 'password'}
           register={register}
           validationSchema={passwordValidationRules}
-          isInvalid={!!errors.password}
-          required
-          autocomplete="current-password"
+          isInvalid={!!errors.newPassword}
+          autocomplete="new-password"
         />
 
-        <button type="button" onClick={() => setIsPasswordVisible(!isPasswordVisible)} className={styles.eye}>
-          <img src={isPasswordVisible ? eyeOn : eyeOff} alt="eye" />
+        <button type="button" onClick={() => setIsNewPasswordVisible(!isNewPasswordVisible)} className={styles.eye}>
+          <img src={isNewPasswordVisible ? eyeOn : eyeOff} alt="eye" />
         </button>
 
-        <p className={styles.passwordError}>{errors?.password?.message}</p>
+        <p className={styles.error}>{errors?.newPassword?.message}</p>
       </div>
       <div className={styles.buttons}>
-        <button type="button" className={styles.submitButton} onClick={resetChanges}>
+        {/* <button type="button" className={styles.submitButton} onClick={resetChanges}>
           {isEditing ? 'Reset' : 'Change'}
-        </button>
+        </button> */}
         <button
-          className={classnames(styles.submitButton, { [styles.hidden]: !isEditing })}
+          // className={classnames(styles.submitButton, { [styles.hidden]: !isEditing })}
+          className={styles.submitButton}
           type="submit"
-          disabled={!isValid || !dataEdited}
+          disabled={!isValid}
         >
           Submit
         </button>
