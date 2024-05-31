a25fade611be64a17158927369823ca09a87f180
Vitali86 <vitali86@tut.by>
2024-05-31 16:11:46 +0300

    feat: add console.log for submit data and button addAddress

--- src/components/ProfileAddresses/ProfileAddresses.tsx
+++ src/components/ProfileAddresses/ProfileAddresses.tsx
@@ -9,7 +9,6 @@ interface AddressesProps {
   customerData: Customer;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const typesAddresses: { [key: string]: string } = {
   shipping: 'addShippingAddressId',
   billing: 'addBillingAddressId',
@@ -21,42 +20,6 @@ enum Types {
 }
 
 export const ProfileAddresses = ({ customerData }: AddressesProps) => {
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   setValue,
-  //   watch,
-  //   formState: { errors, isValid },
-  // } = useForm<CustomerDraft>({ mode: 'all' });
-  const shippingAddresses = findAddresses(customerData.addresses, customerData.shippingAddressIds);
-  const billingAddresses = findAddresses(customerData.addresses, customerData.billingAddressIds);
-  const { defaultBillingAddressId } = customerData;
-  const { defaultShippingAddressId } = customerData;
-  console.log(customerData);
-  console.log(defaultBillingAddressId);
-  console.log(defaultShippingAddressId);
-
-  // console.log(shippingAddresses);
-  // console.log(billingAddresses);
-  // const [isEditing, setIsEditing] = useState(false);
-  // const [dataEdited, setDataEdited] = useState(false);
-
-  // const setInputs = () => {
-  //   setValue('email', customerData.email);
-  //   setValue('firstName', customerData.firstName);
-  //   setValue('lastName', customerData.lastName);
-  //   setValue('dateOfBirth', customerData.dateOfBirth);
-  // };
-
-  // const resetChanges = () => {
-  //   // setInputs();
-  //   setDataEdited(false);
-  //   setIsEditing(!isEditing);
-  // };
-
-  // const onSubmitInfo = () => {};
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const addAddress = async (newAddress: BaseAddress, type: Types) => {
     const result = await sdkService.addAddress({
       version: customerData.version,
@@ -78,6 +41,42 @@ export const ProfileAddresses = ({ customerData }: AddressesProps) => {
       ],
     });
   };
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   setValue,
+  //   watch,
+  //   formState: { errors, isValid },
+  // } = useForm<CustomerDraft>({ mode: 'all' });
+  const shippingAddresses = findAddresses(customerData.addresses, customerData.shippingAddressIds);
+  const billingAddresses = findAddresses(customerData.addresses, customerData.billingAddressIds);
+  const { defaultBillingAddressId } = customerData;
+  const { defaultShippingAddressId } = customerData;
+  console.log(customerData);
+  console.log(defaultBillingAddressId);
+  console.log(defaultShippingAddressId);
+  console.log(typesAddresses);
+  console.log(addAddress);
+  // console.log(shippingAddresses);
+  // console.log(billingAddresses);
+
+  // const [isEditing, setIsEditing] = useState(false);
+  // const [dataEdited, setDataEdited] = useState(false);
+
+  // const setInputs = () => {
+  //   setValue('email', customerData.email);
+  //   setValue('firstName', customerData.firstName);
+  //   setValue('lastName', customerData.lastName);
+  //   setValue('dateOfBirth', customerData.dateOfBirth);
+  // };
+
+  // const resetChanges = () => {
+  //   // setInputs();
+  //   setDataEdited(false);
+  //   setIsEditing(!isEditing);
+  // };
+
+  // const onSubmitInfo = () => {};
 
   // useEffect(() => {
   //   // setInputs();
@@ -87,7 +86,12 @@ export const ProfileAddresses = ({ customerData }: AddressesProps) => {
 
   return (
     <div className={styles.addresses}>
-      <h2 className={styles.accountHeading}>Billing addresses</h2>
+      <div className={styles.addressesHeaderWrapper}>
+        <h2 className={styles.accountHeading}>Billing addresses</h2>
+        <button type="button" className={styles.addNewAddressBtn}>
+          +
+        </button>
+      </div>
       <div className={styles.billings}>
         {shippingAddresses.map(addr => (
           <div key={addr.id} className={styles.addressContainer}>
