f3be4c3e41ad1ccf3ef4b30ff18eca07e2821270
Vital <33701337+Vitali86@users.noreply.github.com>
2024-06-03 21:15:24 +0300

    Feature/rss ecomm 27 product page (#36)
    * feat: add category filter layout
    
    * feat: add breadcrumbs and change components
    
    * feat: products cards
    
    * feat: add name, price and description on product page
    
    * refactor: change price format; add some constants
    
    * refactor: replace style rules
    
    * feat: add buttons and some styles
    
    * feat: adjust the getDollarsFromCents utility and rename it to convertCentsToDollarsString
    
    * refactor: add react-image-gallery
    
    * fix: add configuration for fix 404 page for nested paths
    
    * feat: implement slider
    
    * feat: implement slider full screen mode
    
    * feat: add description for product card
    
    * fix: fix slider image fit
    
    * feat: change logic for filters
    
    * feat: add price filter
    
    * feat: add color filter and button for clearing filters
    
    * feat: implement product variants
    
    * feat: search and sorting componets layout
    
    * refactor: add changes for handle url and breadcrumbs
    
    * feat: add product info section and some style change
    
    * feat: add header
    
    * feat: implement handleAddToCartClick
    
    * feat: add a link to the breadcrumbs
    
    * feat: add functionality to handle location
    
    * feat: add handling slugs for right path to product
    
    * feat: add functionality to change filters const
    
    * feat: add functionality to save filters by url after reloading
    
    * refactor: refactor convertCentsToDollarsString
    
    * feat: add Breadcrumbs component
    
    * feat: update Breadcrumbs
    
    * feat: replace the text attribute color with colored buttons
    
    * refactor: some refactor after merge
    
    * fix: fix catalog filter
    
    * feat: separate the filter component
    
    * feat: add search by search query
    
    * feat: add functionality for sorting
    
    * refactor: add Container
    
    * refactor: add ensureValue utility
    
    * fix: some style edits for adaptivity
    
    * refactor: change Product to ProductProjection
    
    * fix: fix some eslint problem; remove unused code
    
    * fix: remove index.ts and correct imports
    
    * feat: add Loader and handling server errors
    
    * fix: remove unused line
    
    * fix: fix bug when in product card can be set empty category
    
    * feat: implement loader and customToast
    
    * fix: fix loader and styles bugs
    
    * fix: increase dizzy search level
    
    * fix: fix slider adaptive
    
    * fix: fix bugs with price filter
    
    * fix: add placeholder for img in Product Card
    
    * fix: add code with bug when with choosen filters you click on other category
    
    * fix: fix bug with browser popstate
    
    * refactor: eslint disable max-lines-per-function
    
    * refactor: del handleColorClick and rename handleSizeClick to handleSizeOrColorClick
    
    * refactor: rename ensureValue to assertValue, add convertImagesToReactImageGalleryItems function
    
    * refactor: slider is moved to a separate Slider component
    
    * feat: add category validation control
    
    * feat: add state opened for filter when they are active
    
    * fix: fix imports
    
    ---------
    
    Co-authored-by: Yuliya Karuk <karuk.yuliya@gmail.com>

--- src/pages/productItem/productItem.tsx
+++ src/pages/productItem/productItem.tsx
@@ -1,11 +1,179 @@
-import { useParams } from 'react-router-dom';
+import heart from '@assets/heart.svg';
+import { sdkService } from '@commercetool/sdk.service';
+import { ProductProjection, ProductVariant } from '@commercetools/platform-sdk';
+import { Breadcrumbs } from '@components/Breadcrumbs/Breadcrumbs';
+import { Container } from '@components/Container/Container';
+import { Footer } from '@components/Footer/Footer';
+import { Header } from '@components/Header/Header';
+import { Loader } from '@components/Loader/Loader';
+import { ProductAttributes, ProductAttributesView } from '@components/ProductAttributes/ProductAttributesView';
+import { ProductInfoSection } from '@components/ProductInfoSection/ProductInfoSection';
+import { QuantityInput } from '@components/QuantityInput/QuantityInput';
+import { Slider } from '@components/Slider/Slider';
+import { useToast } from '@contexts/toastProvider';
+import { assertValue, convertCentsToDollarsString, convertProductAttributesArrayToObject } from '@utils/utils';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+
+import { useNavigate, useParams } from 'react-router-dom';
+import styles from './productItem.module.scss';
 
 export function ProductItem() {
-  const { slug } = useParams();
-  // тут будет запрос на продукт чтобы получить все данные
+  const { category, subcategory, slug } = useParams();
+
+  assertValue(category, "can't find the product category");
+  assertValue(slug, "can't find the product key (slug)");
+
+  const navigate = useNavigate();
+
+  const [showHeart, setShowHeart] = useState(false);
+  const [product, setProduct] = useState<ProductProjection>({} as ProductProjection);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [activeVariant, setActiveVariant] = useState<ProductVariant>({} as ProductVariant);
+  const [quantity, setQuantity] = useState(1);
+  const { customToast, errorNotify } = useToast();
+
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        setLoading(true);
+
+        const data = await sdkService.getProductProjectionByKey(slug);
+        const { categories } = data;
+        const [category1, category2] = categories;
+
+        const category1Slug = await sdkService.getCategoryById(category1.id).then(cat1 => cat1[0].slug['en-US']);
+        const category2Slug = category2
+          ? await sdkService.getCategoryById(category2.id).then(cat2 => cat2[0].slug['en-US'])
+          : null;
+
+        const isCategoriesCorrect =
+          (category === category1Slug && (!subcategory || subcategory === category2Slug)) ||
+          (category === category2Slug && (!subcategory || subcategory === category1Slug));
+
+        if (!isCategoriesCorrect) {
+          navigate('/404');
+        }
+
+        setProduct(data);
+        setActiveVariant(data.masterVariant);
+
+        setLoading(false);
+      } catch (err) {
+        errorNotify((err as Error).message);
+      }
+    };
+
+    getProduct();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <Loader />
+        {customToast({ position: 'top-center', autoClose: 5000 })};
+      </>
+    );
+  }
+
+  const name = product.name['en-US'];
+  const breadcrumbs: string[] = [];
+  breadcrumbs.push(category);
+  if (subcategory) {
+    breadcrumbs.push(subcategory);
+  }
+  breadcrumbs.push(name);
+
+  const { sku } = activeVariant;
+  const fullPrice: string = activeVariant.prices
+    ? convertCentsToDollarsString(activeVariant.prices[0].value.centAmount)
+    : '';
+  const priceWithDiscount = activeVariant.prices?.[0].discounted?.value.centAmount
+    ? convertCentsToDollarsString(activeVariant.prices[0].discounted.value.centAmount)
+    : '';
+  const hasDiscount = !!activeVariant.prices?.[0].discounted?.value.centAmount;
+  const { images } = product.masterVariant;
+  const { variants } = product;
+  const { attributes } = activeVariant;
+  const allAttributes: ProductAttributes[] = [];
+  if (attributes) {
+    allAttributes.push(convertProductAttributesArrayToObject(product.masterVariant.attributes));
+  }
+  variants.forEach(variant => {
+    if (variant.attributes) {
+      allAttributes.push(convertProductAttributesArrayToObject(variant.attributes));
+    }
+  });
+
+  const handleFavoriteClick = () => {
+    setShowHeart(true);
+    setTimeout(() => {
+      setShowHeart(false);
+    }, 400);
+  };
+
+  const handleAddToCartClick = () => {
+    // const order = {
+    //   action: 'addLineItem',
+    //   productId: product.id,
+    //   variantId: activeVariant.id,
+    //   quantity,
+    // };
+    // console.log(order);
+  };
+
   return (
-    <div>
-      <h1>ProductItem Page {slug}</h1>
+    <div className={styles.pageWrapper}>
+      <Header />
+      <Container>
+        <div className={styles.wrapper}>
+          <Breadcrumbs activeCategorySlug={breadcrumbs} />
+          <div className={styles.productOverview}>
+            <Slider images={images} />
+            <section className={styles.productSummary}>
+              <h2 className={styles.productSummaryHeader}>{name}</h2>
+              {sku ? <div className={styles.sku}>SKU: {sku}</div> : ''}
+              <div className={styles.priceLabel}>
+                Price:
+                <div
+                  className={classNames(styles.fullPrice, {
+                    [styles.fullPriceLineThrough]: hasDiscount,
+                  })}
+                >
+                  {fullPrice}
+                </div>
+                <div className={styles.priceWithDiscount}>{priceWithDiscount}</div>
+              </div>
+
+              <ProductAttributesView
+                activeAttributes={convertProductAttributesArrayToObject(attributes)}
+                allAttributes={allAttributes}
+                setActiveVariant={setActiveVariant}
+                product={product}
+              />
+
+              <div className={styles.buttonsWrapper}>
+                <div className={styles.quantitySelector}>
+                  <QuantityInput value={quantity} onChange={setQuantity} />
+                </div>
+                <div className={styles.buttonsWrapper}>
+                  <button type="button" className={styles.addToCartButton} onClick={handleAddToCartClick}>
+                    add to cart
+                  </button>
+                  <button type="button" className={styles.addToFavoriteButton} onClick={handleFavoriteClick}>
+                    <img src={heart} alt="add to favorite" />
+                    {showHeart && <img className={styles.heartAnimation} src={heart} alt="flying heart" />}
+                  </button>
+                </div>
+              </div>
+              <ProductInfoSection productInfoText={allAttributes[0].details} />
+            </section>
+          </div>
+          <div className={styles.productDescription}>{product.description?.['en-US']}</div>
+        </div>
+      </Container>
+      <Footer />
     </div>
   );
 }
