2e26633324f1069592189f3a72d6b1d7eae9a2f0
Vitali86 <vitali86@tut.by>
2024-06-05 15:42:43 +0300

    feat: cart init commit

--- src/pages/cart/cart.tsx
+++ src/pages/cart/cart.tsx
@@ -1,10 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Container } from '@components/Container/Container';
+import { Footer } from '@components/Footer/Footer';
+import { Header } from '@components/Header/Header';
+import styles from './cart.module.scss';
 
 export function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
-      <Link to="/">Link to Home Page </Link>
-    </div>
+    <>
+      <Header />
+      <Container>
+        <div className={styles.cart}>
+          <div className={styles.myCart}>
+            <h2 className={styles.myCartHeader}>My cart</h2>
+            <div className={styles.productsWrapper}>
+              <div>Product_Cart 1</div>
+              <div>Product_Cart 2</div>
+              <div>Product_Cart 3</div>
+            </div>
+            <div className={styles.promoCode}>PromoCode</div>
+            <div className={styles.note}>Notes Section</div>
+          </div>
+          <div className={styles.orderSummary}>
+            <h2 className={styles.orderSummaryHeader}>Order summary</h2>
+            <div className={styles.productsWrapper}>
+              <div>Product_name 1$</div>
+              <div>Product_name 2$</div>
+              <div>Product_name 3$</div>
+            </div>
+            <p>Total: 70$</p>
+            <button type="button" className={styles.checkoutButton}>
+              Checkout
+            </button>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </>
   );
 }
