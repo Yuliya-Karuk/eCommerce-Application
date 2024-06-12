54060986eece4a1fd3ff284bbd66edf794d1a182
Maxim Ravinskiy <itmaster@duck.com>
2024-06-13 01:57:51 +0300

    feat: The RS School logo has been added

--- src/components/AboutBanner/AboutBanner.tsx
+++ src/components/AboutBanner/AboutBanner.tsx
@@ -1,3 +1,4 @@
+import sprite from '@assets/sprite.svg';
 import { Container } from '@components/Container/Container';
 import { FC } from 'react';
 import styles from './AboutBanner.module.scss';
@@ -7,7 +8,11 @@ const paragraphs = [
   'Sprout remains unique in its pursuit of organic gardening practices, holding true to the belief that organic gardening can make a huge difference in the world. We strive to provide each customer with an organic solution to all plant-related matters, whether it is the soils and fertilizers we use or challenges we may face with insects and disease. We also believe that the art of gardening should be sustainable by nature, and our organic approach helps us achieve this goal of having a positive impact on not only our plants, but also on the Earth and environments that we live in.',
 ];
 
+const rsscoolUrl = 'https://app.rs.school/';
+
 export const AboutBanner: FC = () => {
+  const logoWidth = 150;
+  const logoHeight = 54;
   return (
     <div className={styles.aboutBanner}>
       <Container classname={styles.aboutBanner}>
@@ -20,6 +25,18 @@ export const AboutBanner: FC = () => {
               </p>
             ))}
           </div>
+          <a
+            href={rsscoolUrl}
+            className={styles.aboutBannerLogo}
+            title="Go to RS Scool"
+            aria-label="Go to RS Scool"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <svg width={logoWidth} height={logoHeight}>
+              <use xlinkHref={`${sprite}#rsschool`} />
+            </svg>
+          </a>
         </div>
       </Container>
     </div>
