32d2db45c7e7c18d78a905845bd8faa473e4d195
Maxim Ravinskiy <itmaster@duck.com>
2024-06-16 03:58:50 +0300

    feat: Modal for Promocode

--- src/components/UserMenu/UserMenu.tsx
+++ src/components/UserMenu/UserMenu.tsx
@@ -1,9 +1,10 @@
 import sprite from '@assets/sprite.svg';
 import { sdkService } from '@commercetool/sdk.service';
+import { Modal } from '@components/Modal/Modal';
 import { useAuth } from '@contexts/authProvider';
 import { AppRoutes } from '@router/routes';
 import classnames from 'classnames';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import styles from './UserMenu.module.scss';
 
@@ -14,11 +15,15 @@ enum ToolTips {
   CART = 'Open the cart',
 }
 
+const promoText =
+  "Enjoy 10% off with our summer promo code SUMMER2024, or get 15% off your first order with promo code FIRST! Don't miss out on these great deals!";
+
 export const UserMenu: FC = () => {
   const iconSizeNumber = 26;
   const { isLoggedIn, logout } = useAuth();
   const location = useLocation();
   const isProfile = location.pathname === AppRoutes.PROFILE_ROUTE;
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   const handleLogout = () => {
     sdkService.logoutUser();
@@ -26,47 +31,67 @@ export const UserMenu: FC = () => {
   };
 
   return (
-    <div className={styles.userMenu}>
-      <button
-        type="button"
-        className={classnames(styles.userMenuLogout, { [styles.hidden]: !isLoggedIn })}
-        onClick={handleLogout}
-      >
-        <div className={styles.userMenuLogoutIcon}>
+    <>
+      <div className={styles.userMenu}>
+        <button
+          type="button"
+          className={classnames(styles.userMenuLogout, { [styles.hidden]: !isLoggedIn })}
+          onClick={handleLogout}
+        >
+          <div className={styles.userMenuLogoutIcon}>
+            <svg width={iconSizeNumber} height={iconSizeNumber}>
+              <use xlinkHref={`${sprite}#logout`} />
+            </svg>
+          </div>
+          <span className={styles.userMenuText}>Logout</span>
+        </button>
+
+        <Link
+          to={isProfile ? AppRoutes.HOME_ROUTE : AppRoutes.PROFILE_ROUTE}
+          className={classnames(styles.userMenuIcon, { [styles.hidden]: !isLoggedIn })}
+          aria-label={isProfile ? ToolTips.HOME : ToolTips.PROFILE}
+          data-tooltip={isProfile ? ToolTips.HOME : ToolTips.PROFILE}
+        >
           <svg width={iconSizeNumber} height={iconSizeNumber}>
-            <use xlinkHref={`${sprite}#logout`} />
+            <use xlinkHref={isProfile ? `${sprite}#home` : `${sprite}#profile`} />
           </svg>
-        </div>
-        <span className={styles.userMenuText}>Logout</span>
-      </button>
+        </Link>
 
-      <Link
-        to={isProfile ? AppRoutes.HOME_ROUTE : AppRoutes.PROFILE_ROUTE}
-        className={classnames(styles.userMenuIcon, { [styles.hidden]: !isLoggedIn })}
-        aria-label={isProfile ? ToolTips.HOME : ToolTips.PROFILE}
-        data-tooltip={isProfile ? ToolTips.HOME : ToolTips.PROFILE}
+        <button
+          type="button"
+          className={styles.userMenuIcon}
+          aria-label={ToolTips.PROMO}
+          data-tooltip={ToolTips.PROMO}
+          onClick={e => {
+            e.stopPropagation();
+            setIsOpenModal(true);
+          }}
+        >
+          <svg width={iconSizeNumber} height={iconSizeNumber}>
+            <use xlinkHref={`${sprite}#present`} />
+          </svg>
+        </button>
+
+        <Link
+          to={AppRoutes.CART_ROUTE}
+          className={styles.userMenuIcon}
+          aria-label={ToolTips.CART}
+          data-tooltip={ToolTips.CART}
+        >
+          <svg width={iconSizeNumber} height={iconSizeNumber}>
+            <use xlinkHref={`${sprite}#cart`} />
+          </svg>
+        </Link>
+      </div>
+
+      <Modal
+        isOpenModal={isOpenModal}
+        onClose={() => {
+          setIsOpenModal(false);
+        }}
       >
-        <svg width={iconSizeNumber} height={iconSizeNumber}>
-          <use xlinkHref={isProfile ? `${sprite}#home` : `${sprite}#profile`} />
-        </svg>
-      </Link>
-
-      <button type="button" className={styles.userMenuIcon} aria-label={ToolTips.PROMO} data-tooltip={ToolTips.PROMO}>
-        <svg width={iconSizeNumber} height={iconSizeNumber}>
-          <use xlinkHref={`${sprite}#present`} />
-        </svg>
-      </button>
-
-      <Link
-        to={AppRoutes.CART_ROUTE}
-        className={styles.userMenuIcon}
-        aria-label={ToolTips.CART}
-        data-tooltip={ToolTips.CART}
-      >
-        <svg width={iconSizeNumber} height={iconSizeNumber}>
-          <use xlinkHref={`${sprite}#cart`} />
-        </svg>
-      </Link>
-    </div>
+        {promoText}
+      </Modal>
+    </>
   );
 };
