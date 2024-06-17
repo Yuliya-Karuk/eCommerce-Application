67b0c86a68bbb9365a354fdda7707edfb933286c
Maxim Ravinskiy <itmaster@duck.com>
2024-06-18 01:05:35 +0300

    Revert "fix: The component LinkButton is rewritten"
    This reverts commit b2f8f9315ee6b0247b776e7b945629ec4e36f05e.

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,6 +1,5 @@
-import { AppRoutes } from '@router/routes';
-import { FC, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './LinkButton.module.scss';
 
 interface ILinkButtonProps {
@@ -8,19 +7,9 @@ interface ILinkButtonProps {
 }
 
 export const LinkButton: FC<React.PropsWithChildren & ILinkButtonProps> = ({ to, children, ...props }) => {
-  const navigate = useNavigate();
-  const gotoAboutButtonHandler = useCallback(() => {
-    navigate(AppRoutes.ABOUT_ROUTE);
-    window.scrollTo({
-      top: 0,
-      behavior: 'instant',
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <button type="button" className={styles.linkButton} onClick={gotoAboutButtonHandler} {...props}>
-      About
-    </button>
+    <Link to={to} className={styles.linkButton} {...props}>
+      {children}
+    </Link>
   );
 };
