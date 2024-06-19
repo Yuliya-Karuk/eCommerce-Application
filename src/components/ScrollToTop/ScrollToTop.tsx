eb1a704e09466eef83f8aad1114065daf292b016
Maxim Ravinskiy <itmaster@duck.com>
2024-06-19 18:44:45 +0300

    fix: scrollToTop for filter

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -5,7 +5,7 @@ export const ScrollToTop = () => {
   const route = useLocation();
 
   useEffect(() => {
-    if (window.scrollY > 0) {
+    if (window.scrollY > 0 && !route.search) {
       window.scrollTo({
         top: 0,
         behavior: 'instant',
